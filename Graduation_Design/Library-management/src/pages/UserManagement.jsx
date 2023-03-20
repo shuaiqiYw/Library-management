448ab773bb6b601fe1559b641dc5183ab87061da
shuaiqiYw <1636742864@qq.com>
2023-03-20 22:16:40 +0800

    完善角色信息页面

--- Graduation_Design/Library-management/src/pages/UserManagement.jsx
+++ Graduation_Design/Library-management/src/pages/UserManagement.jsx
@@ -1,36 +1,60 @@
-import React from 'react'
-import { Button, Card, Table } from 'antd'
+import React, {useEffect, useState} from 'react'
+import { Button, Card, Table, Popconfirm } from 'antd'
 import "../assets/css/account.scss"
+import { Link } from "react-router-dom";
+import { getRoleList } from '../API/AxiosURL';
 
-/**
-* @author
-* @function 
-**/
+
 
 const { Column } = Table
 
 export default function UserManagement() {
 
-    let arr = []
+    const [arr, setArr] = useState([])
 
     const handlePage = (val) => {
         console.log(val);
     }
+
+    useEffect(()=>{
+        getRoleList().then(({data}) => {
+            console.log(data);
+            setArr(data)
+        })
+    },[])
 
 
     return (
         <div className='goods'>
             <Card
                 size="small"
-                extra={<Button>添加管理员</Button>}
             >
                 <Table
                     dataSource={arr}
                     onChange={handlePage}
                     rowKey={(record) => { return record._id; }}
                 >
-                    <Column title="用户名" dataIndex="accountName" />
-                    <Column title="操作" dataIndex="accountName" />
+                    <Column title="用户名" dataIndex="roleAccount" />
+                    <Column 
+                        title="操作" 
+                        dataIndex="handler"
+                        render={(text,record,index)=>{
+                            return (
+                                <>
+                                    <Button type="primary" style={{marginLeft: "10px"}}>查看</Button>
+                                    <Button type="primary" style={{marginLeft: "10px"}}><Link to="/home/role">编辑</Link></Button>
+                                    <Popconfirm
+                                        title="你确认删除该管理员吗？"
+                                        okText="确认"
+                                        cancelText="取消"
+                                        disabled={!record.status}
+                                    >
+                                        <Button type="default" style={{marginLeft:"12px",color:"#8c8c8c",padding: "0px 10px"}}>删除</Button>
+                                    </Popconfirm>
+                                </>
+                            )
+                        }}
+                    />
                 </Table>
             </Card>
         </div>
