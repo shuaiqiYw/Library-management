f4d90d46e996308782140b70fc9ed5a642008493
shuaiqiYw <1636742864@qq.com>
2023-03-22 16:19:21 +0800

    编辑用户信息弹框页面完成

--- Graduation_Design/Library-management/src/pages/UserManagement.jsx
+++ Graduation_Design/Library-management/src/pages/UserManagement.jsx
@@ -1,5 +1,6 @@
-import React, {useEffect, useState} from 'react'
-import { Button, Card, Table, Popconfirm, Modal } from 'antd'
+import React, { useEffect, useState } from 'react'
+import { EyeTwoTone, EyeInvisibleOutlined } from '@ant-design/icons';
+import { Button, Card, Table, Popconfirm, Modal, Input } from 'antd'
 import "../assets/css/account.scss"
 import { getRoleList, deleteRole } from '../API/AxiosURL';
 
@@ -15,25 +16,26 @@ export default function UserManagement() {
 
     const [arr, setArr] = useState([])
     const [isModalOpen, setIsModalOpen] = useState(false);
-    
+    const [isModalOpenOfEdit, setIsModalOpenOfEdit] = useState(false)
 
     const handleCancel = () => {
         setIsModalOpen(false);
+        setIsModalOpenOfEdit(false)
     };
 
     const handlePage = (val) => {
         console.log(val);
     }
 
-    useEffect(()=>{
-        getRoleList().then(({data}) => {
+    useEffect(() => {
+        getRoleList().then(({ data }) => {
             console.log(data);
             setArr(data)
         })
-    },[])
+    }, [])
 
-    // 查看
-    const lookInfo = (val) => {
+    // 专门赋值传值
+    const assignment = (val) => {
         var date = new Date(val.roleDate);
         var y = date.getFullYear();
         var m = date.getMonth() + 1;
@@ -43,22 +45,28 @@ export default function UserManagement() {
         var h = date.getHours();
         var minute = date.getMinutes();
         minute = minute < 10 ? ('0' + minute) : minute;
-        let time = y + '-' + m + '-' + d+' '+h+':'+minute; 
+        let time = y + '-' + m + '-' + d + ' ' + h + ':' + minute;
         roleAccount = val.roleAccount
         rolePassword = val.rolePassword
         roleAbout = val.roleAbout
         roleDate = time
+    }
+
+    // 查看
+    const lookInfo = (val) => {
+        assignment(val)
         setIsModalOpen(true);
     }
 
     // 编辑
-    const editInfo = () => {
-        
+    const editInfo = (val) => {
+        assignment(val)
+        setIsModalOpenOfEdit(true)
     }
 
     // 删除
     const deleteInfo = (id) => {
-        deleteRole({id}).then(({data}) => {
+        deleteRole({ id }).then(({ data }) => {
             setArr(data)
         })
     }
@@ -74,39 +82,63 @@ export default function UserManagement() {
                     rowKey={(record) => { return record._id; }}
                 >
                     <Column title="用户名" dataIndex="roleAccount" />
-                    <Column 
-                        title="操作" 
+                    <Column
+                        title="操作"
                         dataIndex="handler"
-                        render={(text,record,index)=>{
+                        render={(text, record, index) => {
                             return (
                                 <>
-                                    <Button type="primary" style={{marginLeft: "10px"}} onClick={()=>{lookInfo(record)}}>查看</Button>
-                                    <Button type="primary" style={{marginLeft: "10px"}} onClick={editInfo}>编辑</Button>
+                                    <Button type="primary" style={{ marginLeft: "10px" }} onClick={() => { lookInfo(record) }}>查看</Button>
+                                    <Button type="primary" style={{ marginLeft: "10px" }} onClick={()=>{editInfo(record)}}>编辑</Button>
                                     <Popconfirm
                                         title="你确认删除该管理员吗？"
                                         okText="确认"
                                         cancelText="取消"
-                                        onConfirm={()=>{deleteInfo(record._id)}}
+                                        onConfirm={() => { deleteInfo(record._id) }}
                                     >
-                                        <Button type="default" style={{marginLeft:"12px",color:"#8c8c8c",padding: "0px 10px"}}>删除</Button>
+                                        <Button type="default" style={{ marginLeft: "12px", color: "#8c8c8c", padding: "0px 10px" }}>删除</Button>
                                     </Popconfirm>
-                                    <Modal
-                                        title="查看信息" 
-                                        open={isModalOpen} 
-                                        onCancel={handleCancel}
-                                        footer={null}
-                                    >
-                                        <p>用户名：{roleAccount}</p>
-                                        <p>密码：{rolePassword}</p>
-                                        <p>授权人：{roleAbout}</p>
-                                        <p>授权时间：{roleDate}</p>
-                                    </Modal>
                                 </>
                             )
                         }}
                     />
                 </Table>
             </Card>
+            <Modal
+                title="查看信息"
+                open={isModalOpen}
+                onCancel={handleCancel}
+                footer={null}
+            >
+                <p>用户名：{roleAccount}</p>
+                <p>密码：{rolePassword}</p>
+                <p>授权人：{roleAbout}</p>
+                <p>授权时间：{roleDate}</p>
+            </Modal>
+            <Modal
+                title="编辑信息"
+                open={isModalOpenOfEdit}
+                onCancel={handleCancel}
+                footer={[
+                    <>
+                        <Button key="back" onClick={handleCancel}>
+                            返回
+                        </Button>,
+                        <Button key="submit" type="primary">
+                            确定
+                        </Button>
+                    </>
+                ]}
+            >
+                <span>用户名：</span>
+                <Input placeholder="请输入" style={{margin:"10px 0"}} value={roleAccount}/>
+                <span>密码：</span>
+                <Input.Password placeholder="请输入" style={{margin:"10px 0"}} value={rolePassword} iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}/>
+                <span>授权人：</span>
+                <Input style={{margin:"10px 0"}} disabled={true} value={roleAbout}/>
+                <span>授权时间：</span>
+                <Input style={{margin:"10px 0"}} disabled={true}  value={roleDate}/>
+            </Modal>
         </div>
     )
 }
